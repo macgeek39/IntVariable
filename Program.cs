9ef1491e94990f6a4a8bfcdc4909c5ed97a91729
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-01 02:04:28 -0600

    Nullable learning

--- Program.cs
+++ Program.cs
@@ -8,3 +8,9 @@
 //Console.WriteLine(isComplete);
 
 
+//Null - lack of value
+int? age = null;
+bool? birthday = null;
+double? battingAverage = null;
+decimal? maxAge = null;
+string firstName = null;
